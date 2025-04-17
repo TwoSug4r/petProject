f50255892c7dbfdc4b13b8710d5550fd2ab4746a
TwoSug4r <nairoz0@mail.ru>
2025-04-17 13:31:53 +0300

    install breeze, delete not needed views, normal:web,auth(routes),Auth,Request(controllers),model

--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,25 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use \App\Http\Controllers\UserController;
-use \App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UserController;
+
+Route::get('/', function () {
+    return view('welcome');
+})->name('welcome');
+
+// Route::get('/', function () {
+//     return view('welcome');
+// })->middleware(['auth', 'verified'])->name('welcome');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::middleware('AdminRoleMiddleware')->group(function () {
+    Route::resource('/users', UserController::class);
+});
 
 require __DIR__.'/auth.php';
-
-Route::get('/', function(){
-    return view('welcome');
-});
-
-//users
-Route::middleware('auth')->group(function(){
-    Route::resource('users', UserController::class);
-});
-
-//profile
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'show'])->name('profile.show');
-});
